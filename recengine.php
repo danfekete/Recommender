f4e9ae1b0fbf0740c3b13e8c7b8bd17709645b87
Daniel Fekete <daniel.fekete@voov.hu>
2016-11-08 16:55:56 +0100

    bomb test

--- recengine.php
+++ recengine.php
@@ -8,16 +8,16 @@
 
 require_once 'vendor/autoload.php';
 
-$data = new \danfekete\Recommender\Models\CSVDataSet('data/naurel.csv');
+$data = new \danfekete\Recommender\Models\CSVDataSet('data/furdogolyo.csv');
 $engine = new \danfekete\Recommender\Engines\JaccardEngine();
-$store = new \danfekete\Recommender\Storages\SqliteStore('data/similar.db');
+$store = new \danfekete\Recommender\Storages\SqliteStore('data/similar_bomb.db');
 
 
 $start = microtime(true);
 $rec = new \danfekete\Recommender\Recommender($engine, $data);
 $rec->setSimilarityStore($store);
 
-$top = $rec->getOne(164)->top(3);
+$top = $rec->getOne(434)->top(3);
 foreach ($top as $item) {
     r($item->getId(), $item->getValue());
 }
