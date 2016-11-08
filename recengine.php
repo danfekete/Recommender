dcd37037371c0cd579ea5edecd86abd553d7c69a
Daniel Fekete <daniel.fekete@voov.hu>
2016-11-08 16:38:59 +0100

    recengine sample

--- recengine.php
+++ recengine.php
@@ -10,9 +10,17 @@ require_once 'vendor/autoload.php';
 
 $data = new \danfekete\Recommender\Models\CSVDataSet('data/naurel.csv');
 $engine = new \danfekete\Recommender\Engines\JaccardEngine();
+$store = new \danfekete\Recommender\Storages\SqliteStore('data/similar.db');
 
+
+$start = microtime(true);
 $rec = new \danfekete\Recommender\Recommender($engine, $data);
-$top = $rec->getOne(1141)->top(3);
+$rec->setSimilarityStore($store);
+
+$top = $rec->getOne(164)->top(3);
 foreach ($top as $item) {
     r($item->getId(), $item->getValue());
 }
+
+
+r(microtime(true) - $start);
